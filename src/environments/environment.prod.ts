7cf2eb0036e8e3d4fb4ec676a362056fb6e240c5
khalil mejri <khalil.mejri.39566@gmail.com>
2020-05-21 11:51:38 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,4 +7,9 @@ export const environment = {
   api_url_users: "http://192.168.0.122:3500/api/users",
   api_url_orders: "http://192.168.0.122:3500/api/orders",
   api_url_tickets: "http://192.168.0.122:3500/api/tickets",
+  api_url_vouchers: "http://192.168.0.122:3500/api/vouchers",
+  api_url_vouchers: "http://192.168.0.122:3500/api/vouchers",
+
+  socketIO_endpoint: "http://localhost:4000"
+    
 };
