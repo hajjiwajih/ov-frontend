85717054ee5fd16a2fadf4ec7a21b350fed3af27
Wajih HAJJI <wajih@hajji.tn>
2020-05-21 12:47:15 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,12 @@ export const environment = {
   enableDebug: false,
   // env [test]
   companyName: "Orange",
-  api_url: "http://192.168.0.122:3500/api",
-  api_url_users: "http://192.168.0.122:3500/api/users",
-  api_url_orders: "http://192.168.0.122:3500/api/orders",
-  api_url_tickets: "http://192.168.0.122:3500/api/tickets",
-  api_url_vouchers: "http://192.168.0.122:3500/api/vouchers",
+  api_url: "http://192.168.0.122:3501/api",
+  api_url_users: "http://192.168.0.122:3501/api/users",
+  api_url_orders: "http://192.168.0.122:3501/api/orders",
+  api_url_tickets: "http://192.168.0.122:3501/api/tickets",
+  api_url_vouchers: "http://192.168.0.122:3501/api/vouchers",
 
-  socketIO_endpoint: "http://localhost:4000"
+  socketIO_endpoint: "http://localhost:4001"
     
 };
