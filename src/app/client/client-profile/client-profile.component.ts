4326e5acd03b36ec000ab26f3a2bf89247e0326e
alijalal-sys <ajmabilal@gmail.com>
2020-06-17 17:05:10 +0200

    edit information added

--- src/app/client/client-profile/client-profile.component.ts
+++ src/app/client/client-profile/client-profile.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { environment } from "./../../../environments/environment";
 import { User } from "./../../models/user";
 import { UserService } from "./../../services/user.service";
+import Swal from "sweetalert2";
+import { Router } from '@angular/router';
 
 @Component({
   selector: "client-profile",
@@ -13,7 +15,7 @@ export class ClientProfileComponent implements OnInit {
   clientId: string;
   currentAgentEmail: string;
   companyName: string;
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService,  private router: Router) {}
 
   ngOnInit() {
     this.clientId = localStorage.getItem("currentUserId");
@@ -24,10 +26,195 @@ export class ClientProfileComponent implements OnInit {
     this.getClientInfo();
   }
 
+  editInfo(attr) {
+    switch (attr) {
+      case "email":
+        this.editMail();
+        break;
+      case "Téléphone":
+        this.editPhone();
+        break;
+    }
+  }
+
   getClientInfo() {
     this.userService.getUserById(this.clientId).subscribe((clientInfo) => {
       this.mClient = clientInfo;
-      console.log(this.mClient)
+      
     });
+  }
+
+  navigate(route) {
+    this.router.navigateByUrl(route);
+  }
+
+  editMail() {
+    console.log(this.mClient)
+    let status = false;
+    Swal.queue([
+      {
+        title: `Tapez votre nouveau Email ?`,
+        text: "Aprés modification vous devez verifier votre mail",
+        input: "email",
+        inputPlaceholder: "Nouvelle valeur",
+        inputValue: this.mClient.email,
+        inputAttributes: {
+          autocapitalize: "off",
+        },
+        showCancelButton: true,
+        cancelButtonText: "Annuler",
+        cancelButtonColor: "#d33",
+        icon: "warning",
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: "Oui, procéder!",
+        showLoaderOnConfirm: true,
+        preConfirm: (change) => {
+          if (change && change != this.mClient.email)
+            return new Promise<any>((resolve, reject) => {
+              this.userService
+                .requestEmailChange(this.mClient.email, change)
+                .subscribe(
+                  (updatedUser) => {
+                    console.log(updatedUser);
+                    resolve(updatedUser);
+                  },
+                  (err) => {
+                    if (err.error.error.code == 422)
+                      reject(
+                        new Error(
+                          "Modification non enregistrée - Email existe déja!"
+                        )
+                      );
+                    else
+                      reject(
+                        new Error(
+                          "Modification non enregistrée - Service non disponible"
+                        )
+                      );
+                  }
+                );
+            })
+              .then(
+                (updated) => {
+                  console.log(updated);
+                  if (!updated) {
+                    throw new Error(
+                      "Operation non terminée - Rollback Operation"
+                    );
+                  } else {
+                    Swal.insertQueueStep({
+                      title: "Terminé",
+                      text: "Modification enregistrée avec succès",
+                      icon: "success",
+                    });
+                    status = true;
+                  }
+                },
+                (error) => {
+                  console.log(error);
+                  Swal.insertQueueStep({
+                    title: "Echec",
+                    text: error,
+                    icon: "error",
+                  });
+                }
+              )
+              .catch((error) => {
+                Swal.insertQueueStep({
+                  title: "Echec",
+                  text: error,
+                  icon: "error",
+                });
+              });
+          else return false;
+        },
+        allowOutsideClick: () => !Swal.isLoading(),
+      },
+    ]).then((result) => {
+      if (status) {
+        this.userService.logout().subscribe((res) => {
+          window.location.reload();
+        });
+      }
+    });
+  }
+
+  editPhone() {
+    Swal.queue([
+      {
+        title: `Tapez votre nouveau Téléphone ?`,
+        input: "number",
+        inputPlaceholder: "Nouvelle valeur",
+        inputValue: this.mClient.phone + "",
+        inputAttributes: {
+          autocapitalize: "off",
+        },
+        showCancelButton: true,
+        cancelButtonText: "Annuler",
+        cancelButtonColor: "#d33",
+        icon: "warning",
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: "Oui, procéder!",
+        showLoaderOnConfirm: true,
+        inputValidator: (value) => {
+          if (!value) {
+            return "Vous devez spécifier la raison SVP!";
+          } else if (value.length != 8) {
+            return "Vous devez spécifier le téléphone correctement!";
+          }
+        },
+        preConfirm: (change) => {
+          if (change)
+            return new Promise<any>((resolve, reject) => {
+              this.mClient.phone = change;
+              this.userService.updateUser(this.mClient, this.mClient.id).subscribe(
+                (updatedUser) => {
+                  console.log(updatedUser);
+                  resolve(updatedUser);
+                },
+                (err) => {
+                  reject(
+                    new Error(
+                      "Modification non enregistrée - Service non-disponible"
+                    )
+                  );
+                }
+              );
+            })
+              .then(
+                (updated) => {
+                  console.log(updated);
+                  if (!updated) {
+                    throw new Error(
+                      "Operation non terminée - Rollback Operation"
+                    );
+                  } else {
+                    Swal.insertQueueStep({
+                      title: "Terminé",
+                      text: "Modification enregistrée avec succès",
+                      icon: "success",
+                    });
+                  }
+                },
+                (error) => {
+                  console.log(error);
+                  Swal.insertQueueStep({
+                    title: "Echec",
+                    text: error,
+                    icon: "error",
+                  });
+                }
+              )
+              .catch((error) => {
+                Swal.insertQueueStep({
+                  title: "Echec",
+                  text: error,
+                  icon: "error",
+                });
+              });
+        },
+        allowOutsideClick: () => !Swal.isLoading(),
+      },
+    ]).then((result) => {});
   }
 }
