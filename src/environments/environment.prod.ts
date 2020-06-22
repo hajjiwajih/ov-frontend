2ff9818034ea238bf24917a87f2615cadcb1cc25
Khalil Mejri <khalil.mejri.39566@gmail.com>
2020-06-22 18:00:31 +0100

    update angular env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,12 +4,12 @@ export const environment = {
   // env [test]
   companyName: "Symtelecom",
   // api routes
-  api_url: "https://ditriot.com:9443/api",
-  api_url_users: "https://ditriot.com:9443/api/users",
-  api_url_orders: "https://ditriot.com:9443/api/orders",
-  api_url_tickets: "https://ditriot.com:9443/api/tickets",
-  api_url_vouchers: "https://ditriot.com:9443/api/vouchers",
+  api_url: "https://www.symvoucher.com/api",
+  api_url_users: "https://www.symvoucher.com/api/users",
+  api_url_orders: "https://www.symvoucher.com/api/orders",
+  api_url_tickets: "https://www.symvoucher.com/api/tickets",
+  api_url_vouchers: "https://www.symvoucher.com/api/vouchers",
   // socket server
-  socketIO_endpoint: "https://ditriot.com:9443",
+  socketIO_endpoint: "https://www.symvoucher.com",
 
 };
