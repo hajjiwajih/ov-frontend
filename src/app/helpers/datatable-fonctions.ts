c5a98a35c6ca5928aa15a01663ecbd20183c8b32
alijalal-sys <ajmabilal@gmail.com>
2020-06-24 17:36:57 +0200

    add responsive classes to actions buttons

--- src/app/helpers/datatable-fonctions.ts
+++ src/app/helpers/datatable-fonctions.ts
@@ -19,7 +19,7 @@ export function appendDtActions(aData, nRow, isClient, args) {
     if (args.valid)
       $(`td:eq(${args.column})`, nRow).append(
         `<button
-      class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-primary collapsed"
+      class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-primary collapsed actions-button"
       aria-expanded="false"
       (click)="showDialog(order)"
       >
@@ -27,7 +27,7 @@ export function appendDtActions(aData, nRow, isClient, args) {
       </button>
 
       <button
-      class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-focus collapsed"
+      class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-focus collapsed actions-button"
       aria-expanded="false"
       (click)="downloadCSV(order)"
       >
@@ -38,7 +38,7 @@ export function appendDtActions(aData, nRow, isClient, args) {
     else
       $(`td:eq(${args.column})`, nRow).append(
         `<button
-      class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-primary collapsed"
+      class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-primary collapsed actions-button"
       aria-expanded="false"
       (click)="showDialog(order)"
       >
@@ -52,7 +52,7 @@ export function appendDtActions(aData, nRow, isClient, args) {
     if (args.valid || args.reject)
       $(`td:eq(${args.column})`, nRow).append(
         `<button
-        class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-primary collapsed"
+        class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-primary collapsed actions-button"
         aria-expanded="false"
         (click)="showDialog(order)"
       >
@@ -62,7 +62,7 @@ export function appendDtActions(aData, nRow, isClient, args) {
     else
       $(`td:eq(${args.column})`, nRow).append(
         `<button
-        class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-primary collapsed"
+        class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-primary collapsed actions-button"
         aria-expanded="false"
         (click)="showDialog(order)"
       >
@@ -70,14 +70,14 @@ export function appendDtActions(aData, nRow, isClient, args) {
       </button>
 
       <button
-        class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-success collapsed"
+        class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-success collapsed actions-button"
         aria-expanded="false"
         (click)="validateOrder(order)"
       >
         <i class="pe-7s-key btn-icon-wrapper"> </i>Valider
       </button>
       <button
-        class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-danger collapsed"
+        class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-danger collapsed actions-button"
         aria-expanded="false"
         (click)="rejectOrder(order)"
       >
@@ -100,21 +100,21 @@ export function appendDtAdminActions(aData, nRow, args) {
   if (args.valid)
     $(`td:eq(${args.column})`, nRow).append(
       `<button
-    class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-primary collapsed"
+    class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-primary collapsed actions-button"
     aria-expanded="false"
     >
     <i class="pe-7s-upload btn-icon-wrapper"> </i>Aperçu
     </button>
 
     <button disabled
-    class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-focus collapsed"
+    class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-focus collapsed actions-button"
     aria-expanded="false"
     >
     <i class="pe-7s-check btn-icon-wrapper"> </i
     >Valider
     </button>
     <button disabled
-    class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-danger collapsed"
+    class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-danger collapsed actions-button"
     aria-expanded="false"
     >
     <i class="pe-7s-close btn-icon-wrapper"> </i
@@ -124,21 +124,21 @@ export function appendDtAdminActions(aData, nRow, args) {
   else
     $(`td:eq(${args.column})`, nRow).append(
       `<button
-      class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-primary collapsed"
+      class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-primary collapsed actions-button"
       aria-expanded="false"
       >
       <i class="pe-7s-upload btn-icon-wrapper"> </i>Aperçu
       </button>
   
       <button
-      class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-focus collapsed"
+      class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-focus collapsed actions-button"
       aria-expanded="false"
       >
       <i class="pe-7s-check btn-icon-wrapper"> </i
       >Valider
       </button>
       <button 
-    class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-danger collapsed"
+    class="ml-2 mr-2 btn-icon btn-transition btn btn-outline-danger collapsed actions-button"
     aria-expanded="false"
     >
     <i class="pe-7s-close btn-icon-wrapper"> </i
