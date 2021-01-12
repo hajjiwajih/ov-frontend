40957b539cd8fbb03a5a31ebc7a693a62371009b
Anis-Abaza <62609780+Anis-Abaza@users.noreply.github.com>
2021-01-12 19:43:03 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,11 +4,11 @@ export const environment = {
   // env [test]
   companyName: "Symtelecom",
   // api routes
-  api_url: "http://ov-backend-test.192.168.0.142.nip.io/api",
-  api_url_users: "http://ov-backend-test.192.168.0.142.nip.io/api/users",
-  api_url_orders: "http://ov-backend-test.192.168.0.142.nip.io/api/orders",
-  api_url_tickets: "http://ov-backend-test.192.168.0.142.nip.io/api/tickets",
-  api_url_vouchers: "http://ov-backend-test.192.168.0.142.nip.io/api/vouchers",
+  api_url: "http://172.30.69.61/api",
+  api_url_users: "http://172.30.69.61/api/users",
+  api_url_orders: "http://172.30.69.61/api/orders",
+  api_url_tickets: "http://172.30.69.61/api/tickets",
+  api_url_vouchers: "http://172.30.69.61/api/vouchers",
   // socket server
   socketIO_endpoint: "http://socket-server-test.192.168.0.142.nip.io",
 
