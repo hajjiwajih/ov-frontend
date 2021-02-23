8b3b42ef359ca6a1bc5ec819b82480a0f4e12dbc
Anis-Abaza <62609780+Anis-Abaza@users.noreply.github.com>
2021-02-23 10:46:19 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,11 +4,11 @@ export const environment = {
   // env [test]
   companyName: "Symtelecom",
   // api routes
-  api_url: "http://ov-backend-voucherapp.192.168.20.12.nip.io/api",
-  api_url_users: "http://ov-backend-voucherapp.192.168.20.12.nip.io/api/users",
-  api_url_orders: "http://ov-backend-voucherapp.192.168.20.12.nip.io/api/orders",
-  api_url_tickets: "http://ov-backend-voucherapp.192.168.20.12.nip.io/api/tickets",
-  api_url_vouchers: "http://ov-backend-voucherapp.192.168.20.12.nip.io/api/vouchers",
+  api_url: "http://ov-backend-voucherapp.192.168.20.12.xip.io/api",
+  api_url_users: "http://ov-backend-voucherapp.192.168.20.12.xip.io/api/users",
+  api_url_orders: "http://ov-backend-voucherapp.192.168.20.12.xip.io/api/orders",
+  api_url_tickets: "http://ov-backend-voucherapp.192.168.20.12.xip.io/api/tickets",
+  api_url_vouchers: "http://ov-backend-voucherapp.192.168.20.12.xip.io/api/vouchers",
   // socket server
   socketIO_endpoint: "http://socket-server-voucherapp.192.168.20.12.xip.io",
 
