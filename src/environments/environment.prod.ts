ad605d1bb4c41e1b3d25e6ad08df7e2912f40c56
Anis-Abaza <62609780+Anis-Abaza@users.noreply.github.com>
2021-03-29 15:51:51 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,13 +4,13 @@ export const environment = {
   // env [test]
   companyName: "Symtelecom",
   // api routes
-  api_url: "http://ov-backend-voucherapp.192.168.20.12.nip.io/api",
-  api_url_users: "http://ov-backend-voucherapp.192.168.20.12.nip.io/api/users",
-  api_url_orders: "http://ov-backend-voucherapp.192.168.20.12.nip.io/api/orders",
-  api_url_tickets: "http://ov-backend-voucherapp.192.168.20.12.nip.io/api/tickets",
-  api_url_vouchers: "http://ov-backend-voucherapp.192.168.20.12.nip.io/api/vouchers",
+  api_url: "http://ov-backend-symvoucher.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud/api",
+  api_url_users: "http://ov-backend-symvoucher.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud",
+  api_url_orders: "http://ov-backend-symvoucher.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud/api/orders",
+  api_url_tickets: "http://ov-backend-symvoucher.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud/api/tickets",
+  api_url_vouchers: "http://ov-backend-symvoucher.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud/api/vouchers",
   // socket server
-  socketIO_endpoint: "http://socket-server-voucherapp.192.168.20.12.nip.io",
+  socketIO_endpoint: "http://socket-server-symvoucher.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud",
 
 };
 //http://socket-server-voucherapp.192.168.20.12.nip.io
