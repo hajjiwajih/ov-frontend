14cdf73c4feebdc2bfe418f006d7fa3c7ad353bf
Anis-Abaza <62609780+Anis-Abaza@users.noreply.github.com>
2021-03-31 14:50:27 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,7 +11,7 @@ export const environment = {
   api_url_vouchers: "http://172.21.95.232/api/vouchers",
   // socket server
   //socketIO_endpoint: "http://172.21.168.197",
-  socketIO_endpoint: "socket-server-test.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud",
+  socketIO_endpoint: "http://172.21.168.197",
 
 };
 //http://socket-server-voucherapp.192.168.20.12.nip.io
