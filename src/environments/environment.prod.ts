022be7f3cd527f0c682a23080ed9b946928395ce
Anis-Abaza <62609780+Anis-Abaza@users.noreply.github.com>
2021-03-31 22:51:11 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,14 +4,14 @@ export const environment = {
   // env [test]
   companyName: "Symtelecom",
   // api routes
-  api_url: "ov-backend-symtest.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud/api",
-  api_url_users: "ov-backend-symtest.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud/api/users",
-  api_url_orders: "ov-backend-symtest.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud/api/orders",
-  api_url_tickets: "ov-backend-symtest.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud/api/tickets",
-  api_url_vouchers: "ov-backend-symtest.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud/api/vouchers",
+  api_url: "ov-backend-vouchertest.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud/api",
+  api_url_users: "ov-backend-vouchertest.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud/api/users",
+  api_url_orders: "ov-backend-vouchertest.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud/api/orders",
+  api_url_tickets: "ov-backend-vouchertest.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud/api/tickets",
+  api_url_vouchers: "ov-backend-vouchertest.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud/api/vouchers",
   // socket server
   //socketIO_endpoint: "http://172.21.168.197",
-  socketIO_endpoint: "socket-server-symtest.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud",
+  socketIO_endpoint: "socket-server-vouchertest.symvoucherpreprod-32b3324939abd6931214ec409bc8bcf6-0000.eu-de.containers.appdomain.cloud",
 
 };
 //http://socket-server-voucherapp.192.168.20.12.nip.io
